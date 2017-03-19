963a05d93ba21e672df59946ccef9c41bb243728
Lee Briggs <lbriggs@apptio.com>
2017-03-19 21:54:50 +0000

    Bump version

--- unseal.rb
+++ unseal.rb
@@ -7,11 +7,11 @@
 class Unseal < Formula
   desc "Unseal multiple vault servers quickly and easily"
   homepage "https://github.com/jaxxstorm/unseal"
-  version "v0.3-beta"
+  version "v0.3.1"
 
   if Hardware::CPU.is_64_bit?
-    url "https://github.com/jaxxstorm/unseal/releases/download/v0.3-beta/unseal-v0.3-beta_darwin_amd64"
-    sha256 "f25061683b741ad394efd4277bb076a7a1c095b98a53c76470da70aa37f08d8e"
+    url "https://github.com/jaxxstorm/unseal/releases/download/v0.3.1/unseal_0.3.1_darwin_amd64.zip"
+    sha256 "1e6bedf993dcb50022d011af9d4fc6450e907e77715acf8ed4a83b0ae489bbe1"
   #else
   #  url "https://github.com/jaxxstorm/unseal/releases/download/v0.3-beta/unseal-v0.3-beta_darwin_amd64"
   #  sha256 ""
